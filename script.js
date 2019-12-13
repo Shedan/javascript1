56db572001e50af6936717b398063f75f468f18c
Artem <shedanua@gmail.com>
2019-12-13 11:13:40 +0200

    This is first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// // var sayHelloTo = function (name) {
+// //     console.log('Привет ' + name)
+// // }
+
+
+
+
+// // // var carName = 'Ford'
+// // // var personName = 'Name'
+// // // var carYear = 2008
+// // // var personYear = 1990
+
+// // // function calculateAge (year) {
+// // //     var currentYear = 2019
+// // //     var result = currentYear - year
+// // //     return result
+// // // }
+
+// // // function checkAndLogAge (year, name, compareTo) {
+// // //     if (calculateAge(year) <  compareTo){
+// // //         console.log('Возраст ' + name + ' меньше ' + compareTo + ' лет')
+// // //     } else {
+// // //         console.log('Возраст ' + name + ' больше ' + compareTo + ' лет')
+// // //     }    
+// // // }
+
+// // // checkAndLogAge (carYear, 'машины', 8)
+// // // checkAndLogAge (personYear, 'человека', 30)
+
+
+// function User() { }
+// User.prototype = { admin: false };
+
+// let user = new User();
+
+// User.prototype = { admin: true };
+
+// alert(user.admin);
+
+let persone = {
+    name: 'Jhone',
+    age: 25,
+    isMarried: false
+}
+
+let answer = prompt("Are you 21?", "yes");
